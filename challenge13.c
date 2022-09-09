e44af62ab9049b114ad55be33e5c2ed3f1781b00
Ielmarzouqy <elmarzouqyimane@gmail.com>
2022-09-09 09:43:42 +0100

    challenge 13

---
+++ challenge13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int number;
+
+printf("Entrer un nombre: ");
+scanf("%d", &number);
+printf("valeur équivalente en octal est: %o", number);
+printf("\nla valeur équivalente en hexadécimal.%x", number);
+
+
+
+    return 0;
+}
